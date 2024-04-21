029cddac6a55a085a788876c4e165d1ba9875d83
トム・クルーズ <ryu@tomukuruzunonotobukkukonpyuta.local>
2024-04-21 23:37:13 +0900

    入力フォーム作成（statefullWidgetを作成）

--- testtodoapp/lib/listadd.dart
+++ testtodoapp/lib/listadd.dart
@@ -1,18 +1,72 @@
 import 'package:flutter/material.dart';
 
+class TodoAddPage extends StatefulWidget {
+  @override
+  _TodoAddPageState createState() => _TodoAddPageState();
+}
+
 // リスト一覧画面用Widget
-class TodoAddPage extends StatelessWidget {
+class _TodoAddPageState extends State<TodoAddPage> {
+  // 入力されたテキストをデータとして持つ
+  String _text = '';
+
+  // データを元に表示するWidget
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: TextButton(
-          // ボタンをクリックした時の処理
-          onPressed: () {
-            // "pop"で前の画面に戻る
-            Navigator.of(context).pop();
-          },
-          child: Text('リスト追加画面(クリックで戻る)'),
+      appBar: AppBar(
+        title: Text('リスト追加'),
+      ),
+
+      body: Container(
+        // 余白をつける
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // 入力されたテキストを表示
+            Text(_text, style: TextStyle(
+              fontSize: 50,
+              color: Colors.blue
+              )),
+            const SizedBox(height: 8),
+            // テキスト入力
+            TextField(
+              // 入力されたテキストの値を受け取る（valueが入力されたテキスト）
+              onChanged: (String value) {
+                // データが変更したことを知らせる（画面を更新する）
+                setState(() {
+                  // データを変更
+                  _text = value;
+                });
+              },
+            ),
+            const SizedBox(height: 8),
+            Container(
+              // 横幅いっぱいに広げる
+              width: double.infinity,
+              // リスト追加ボタン
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('リスト追加', style: TextStyle(color: Colors.blue)),
+               ),
+              ),
+              const SizedBox(height: 8),
+
+              Container(
+                // 横幅いっぱいに広げる
+                width: double.infinity,
+                // キャンセルボタン
+                child: TextButton(
+                  // ボタンをクリックした時の処理
+                  onPressed: () {
+                    // popで前の画面に戻る
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('キャンセル'),
+                ),
+              )
+          ],
         ),
       ),
     );
